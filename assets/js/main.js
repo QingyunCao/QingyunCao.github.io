a7e2accf3b256adb9887170af443ace9956e4fd7
QingyunCao <746658985@qq.com>
2021-10-19 19:31:17 +0200

    []

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,21 @@
 			'xlarge-to-max':    '(min-width: 1681px)',
 			'small-to-xlarge':  '(min-width: 481px) and (max-width: 1680px)'
 		});
+        //var audio = document.getElementById('music1');
+ 
+         $("#btn").bind("touchstart", function bf() {
+         if(audio !== null) {
+         //检测播放是否已暂停.audio.paused 在播放器播放时返回false.
+         //alert(audio.paused);
+         if(audio.paused) {
+         audio.play(); //audio.play();// 这个就是播放
+         $("#btn").addClass("active")
+         } else {
+         audio.pause(); // 这个就是暂停
+         $("#btn").removeClass("active")
+         }
+         }
+         })
 
 	// Stops animations/transitions until the page has ...
 
